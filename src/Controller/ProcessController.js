2c84bfeb263b8d126177fc080d5884b6934138aa
Lucas Vieira <lucas.engen@outlook.com>
2021-09-14 21:23:27 -0300

    fga-eps-mds/2021.1-Oraculo#34 Adds functions prototypes to process controller
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

---
+++ src/Controller/ProcessController.js
@@ -0,0 +1,18 @@
+
+
+async function getProcessByID(id) {
+
+}
+
+async function getAllProcesses() {
+
+}
+
+async function createProcess(process) {
+
+}
+
+modules.exports = {
+    getProcessByID,
+    getAllProcesses,
+};
