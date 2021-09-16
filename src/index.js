b6e2b21ff0bfbd2743de896cccc86eb72b4c636a
Lucas Vieira <lucas.engen@outlook.com>
2021-09-16 02:48:08 -0300

    fga-eps-mds/2021.1-Oraculo#34 Fixes some syntax errors - Adds more database initialization options - Improves exception handling while authenticating with database
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,37 +1,44 @@
-const { express } = require('express');
-const { cors } = require('cors');
-const { routes } = require('./routes');
-const { Sequelize } = require('sequelize');
-const { env } = require('dotenv');
-const { Process } = require('./Model/Process');
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const {Sequelize} = require('sequelize');
+const env  = require('dotenv');
+const Process = require('./Model/Process');
 
 env.config();
 
 const {
     DB_USER,
     DB_PASS,
-    DB_HOSTNAME,
+    DB_HOST,
     DB_PORT,
     DB_NAME,
+    APP_PORT,
 } = process.env;
 
 const app = express();
 
 const db = new Sequelize({
+        username: `${DB_USER}`,
+        password: `${DB_PASS}`,
+        database: `${DB_NAME}`,
+        port: `${DB_PORT}`,
         dialect: "postgres",
-        host: `postgres://${DB_USER}:${DB_PASS}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`
+        host: Number.parseInt(`${DB_HOST}`),
+        define: {
+            timestamps: true,
+            underscored: true,
+        }
     }
 );
 
+db.authenticate().catch((reason) => {
+    console.error(`failed to authenticate: ${reason}`);
+});
+
 Process.init(db);
 
-try {
-    db.authenticate();
-} catch(error) {
-    console.error(`failed to connect with database: ${error}`);
-}
-
-app.listen(8000);
+app.listen(APP_PORT);
 app.use(cors());
 app.use(express.json());
 app.use(routes);
