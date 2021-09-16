24da851bb2798436137d30b933b28ad73d54dc42
Lucas Vieira <lucas.engen@outlook.com>
2021-09-16 02:37:33 -0300

    fga-eps-mds/2021.1-Oraculo#34 Renames routes and removes a useless require
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,11 @@
-const { app } = require('./index');
 const express = require('express');
 const ProcessController = require('./Controller/ProcessController');
 const check  = require('./Utils/jwt');
 
 const routes = express.Router();
 
-routes.get('/processes', check.verifyJWT, ProcessController.getAllProcesses);
-routes.get('/processes/:id/', check.verifyJWT, ProcessController.getProcessByID);
-routes.post('/processes', check.verifyJWT, ProcessController.createProcess);
+routes.get('/processos', check.verifyJWT, ProcessController.getAllProcesses);
+routes.get('/processos/:id/', check.verifyJWT, ProcessController.getProcessByID);
+routes.post('/processos', check.verifyJWT, ProcessController.createProcess);
 
 module.exports = routes;
