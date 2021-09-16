d49d35e425d768c73fcb2c7d996864c2530c8d21
Lucas Vieira <lucas.engen@outlook.com>
2021-09-16 02:33:41 -0300

    fga-eps-mds/2021.1-Oraculo#34 Implements functions to create and write processes
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- src/Controller/ProcessController.js
+++ src/Controller/ProcessController.js
@@ -1,18 +1,70 @@
+const Process = require('../Model/Process');
 
+async function getProcessByID(request, response) {
+    
+    const { id } = request.params;
 
-async function getProcessByID(id) {
+    try {
+        const process = await Process.findOne(id);
 
+        return response.json(process);
+    } catch(failure) {
+        console.error(`failed to get process by id: ${failure}`);
+
+        return response.status(400).json({error: "Invalid ID"});
+    }
 }
 
-async function getAllProcesses() {
+async function getAllProcesses(request, response) {
 
+    try {
+        const processes = await Process.findAll();
+
+        return response.json(processes);
+    } catch(failure) {
+        console.error(`failed to get all processes: ${failure}`);
+
+        return response.status(400).json({error: "Could not find processes"});
+    }
 }
 
-async function createProcess(process) {
+async function createProcess(request, response) {
 
+    const { 
+        regNum,
+        originLocation,
+        location,
+        sourceDocument,
+        documentDescription,
+        seiNumber,
+        receivedBy,
+        answerDocument,
+        contactInfo,
+    } = request.params;
+
+
+    try {
+        const createdProcess = await Process.create({
+            regNum,
+            originLocation,
+            location,
+            sourceDocument,
+            documentDescription,
+            seiNumber,
+            receivedBy,
+            answerDocument,
+            contactInfo,
+        });
+
+        return response.json(createdProcess);
+    } catch(error) {
+
+        return response.status(500).json({error: "could not insert process into database"});
+    }
 }
 
-modules.exports = {
+module.exports = {
     getProcessByID,
     getAllProcesses,
+    createProcess,
 };
