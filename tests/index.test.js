7f3c4da9b71ef179b423913c63af39ae2ce3a19d
Lucas Vieira <lucas.engen@outlook.com>
2021-09-17 17:58:20 -0300

    Fixes expected HTTP response codes #34
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,12 @@
-const app = require('../src/index');
-const request = require('supertest');
+const app = require("../src/index");
+const request = require("supertest");
 
-test('Test routes related to processes', async () => {
+test("Test routes related to processes", async () => {
+	const r1 = await request(app).get("/processos");
+	const r2 = await request(app).get("/processos/0");
+	const r3 = await request(app).post("/processos");
 
-  const r1 = await request(app).get("/processos");
-  const r2 = await request(app).get("/processos/0");
-  const r3 = await request(app).post("/processos");
-
-  expect(r1.statusCode).toBe(400);
-  expect(r2.statusCode).toBe(400);
-  expect(r3.statusCode).toBe(500);
+	expect(r1.statusCode).toBe(401);
+	expect(r2.statusCode).toBe(401);
+	expect(r3.statusCode).toBe(401);
 });
