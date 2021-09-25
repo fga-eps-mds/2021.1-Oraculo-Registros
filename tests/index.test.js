b0e1000820e03290c15bb71fab4d103b86ef1c00
Lucas Vieira <lucas.engen@outlook.com>
2021-09-25 17:30:52 -0300

    fix: fixes some tests
    Signed-off-by: Lucas Vieira <lucas.engen@outlook.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,23 +4,3 @@ test("Test express server app", (done) => {
 	expect(app).toBeDefined();
 	done();
 });
-
-/*
-const request = require("supertest");
-describe("Test processes routes", () => {
-	it("GET /processos", async () => {
-		const r1 = await request(app).get("/processos");
-		expect(r1.statusCode).toEqual(401);
-	});
-
-	it("GET /processos/0", async () => {
-		const r2 = await request(app).get("/processos/0");
-		expect(r2.statusCode).toEqual(401);
-	});
-
-	it("POST /processos", async () => {
-		const r3 = await request(app).post("/processos");
-		expect(r3.statusCode).toEqual(401);
-	});
-});
-*/
