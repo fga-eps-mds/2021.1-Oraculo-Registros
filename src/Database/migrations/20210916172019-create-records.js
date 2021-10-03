08e0ec90cedbca2ad63d5a0f4f2fd055db4a7dc1
Lucas Vieira <lucas.engen@outlook.com>
2021-10-02 22:11:53 -0300

    fga-eps-mds/2021.1-Oraculo#34 chore: :zap: updates database model
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

---
+++ src/Database/migrations/20210916172019-create-records.js
@@ -0,0 +1,97 @@
+"use strict";
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+
+        return queryInterface.createTable("records", {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true,
+            },
+            register_number: {
+                type: Sequelize.DataTypes.TEXT,
+                allowNull: true,
+                unique: true,
+            },
+            inclusion_date: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            city: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            state: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            requester: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            document_type: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            document_number: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            document_date: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            description: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            sei_number: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            receipt_form: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            contact_info: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            situation: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            created_by: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+
+        return queryInterface.dropTable("records");
+    },
+};
