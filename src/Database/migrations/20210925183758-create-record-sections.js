b2d15ec5381702f780a974d87f12aaaa5e917618
Lucas Vieira <lucas.engen@outlook.com>
2021-10-09 19:29:09 -0300

    fga-eps-mds/2021.1-Oraculo#35 feat: :sparkles: updates database model - adds new migrations to store sections and records section history
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

---
+++ src/Database/migrations/20210925183758-create-record-sections.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        return queryInterface.createTable("record_sections", {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            record_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: "records", key: "id" },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+            },
+            section_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: "sections", key: "id" },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        queryInterface.dropTable("record_sections");
+    },
+};
