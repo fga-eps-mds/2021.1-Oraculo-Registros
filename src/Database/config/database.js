179a29cadbc9e8e35b2d57ff1e0d28afdb001ef1
Lucas Vieira <lucas.engen@outlook.com>
2021-10-09 19:53:34 -0300

    fga-eps-mds/2021.1-Oraculo#35 chore: updates database configuration
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- src/Database/config/database.js
+++ src/Database/config/database.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
-const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
 
 module.exports = {
     username: `${DB_USER}`,
     password: `${DB_PASS}`,
     database: `${DB_NAME}`,
-    port: `${DB_PORT}`,
+    port: 5432,
     dialect: "postgres",
     host: `${DB_HOST}`, //Number.parseInt(`${DB_HOST}`),
     define: {
