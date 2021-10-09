e28c3299f3e801064bf9486e6614662363903c6d
Lucas Vieira <lucas.engen@outlook.com>
2021-10-09 19:33:26 -0300

    fga-eps-mds/2021.1-Oraculo#35 feat: :sparkles: adds new routes - adds a middleware to validate request body
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,21 @@ const RecordController = require("./Controller/RecordController");
 
 const routes = express.Router();
 
+async function parseBody(req, res, next) {
+    try {
+        JSON.parse(req.body);
+        next();
+    } catch (err) {
+        return res
+            .status(400)
+            .json({ error: "invalid request, please check JSON syntax" });
+    }
+}
+
 routes.get("/records", RecordController.getAllRecords);
 routes.get("/records/:id/", RecordController.getRecordByID);
-routes.post("/records", RecordController.createRecord);
+routes.post("/records", parseBody, RecordController.createRecord);
+routes.post("/records/:id/forward", parseBody, RecordController.forwardRecord);
+routes.get("/records/:id/sections", RecordController.getRecordSectionsByID);
 
 module.exports = routes;
