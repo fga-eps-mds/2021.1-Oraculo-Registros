50fdcd81284bcd07dc7099e288193b937c54cd6c
Lucas Vieira <lucas.engen@outlook.com>
2021-10-12 01:34:40 -0300

    fga-eps-mds/2021.1-Oraculo#99 feat: :sparkles: adds route to get records by pages
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 routes.get("/records", RecordController.getAllRecords);
 routes.get("/records/:id/", RecordController.getRecordByID);
 routes.post("/records", RecordController.createRecord);
+routes.get("/records/page/:page", RecordController.getRecordsByPage);
 
 module.exports = routes;
