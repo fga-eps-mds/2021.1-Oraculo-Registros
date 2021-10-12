f6cadef9e9622dba14bc90e27ed580826d80ad92
Lucas Vieira <lucas.engen@outlook.com>
2021-10-12 01:35:38 -0300

    fga-eps-mds/2021.1-Oraculo#99 feat: :sparkles: implements method to paginate queries - fix: :bug: fixes HTTP response code 400 -> 204 - refactor: :art: improves code format
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- src/Controller/RecordController.js
+++ src/Controller/RecordController.js
@@ -2,82 +2,98 @@ const Record = require("../Model/Record");
 const { recordStatus } = require("../Model/Situation");
 
 function generateRegisterNumber() {
-    const date = new Date();
-    const seq = date.getTime();
-    return `${Math.round(seq)}/${date.getFullYear()}`;
+  const date = new Date();
+  const seq = date.getTime();
+  return `${Math.round(seq)}/${date.getFullYear()}`;
 }
 
 async function getRecordByID(request, response) {
-    const { id } = request.params;
+  const { id } = request.params;
 
-    const record = await Record.findByPk(id);
-    if (!record) {
-        return response
-            .status(400)
-            .json({ error: `Could not find record with id ${id}` });
-    }
+  const record = await Record.findByPk(id);
+  if (!record) {
+    return response.status(400).json({ error: `Could not find record with id ${id}` });
+  }
 
-    return response.json(record);
+  return response.json(record);
 }
 
 async function getAllRecords(request, response) {
-    try {
-        const records = await Record.findAll();
-        if (!records.length) {
-            return response.status(400).json({ error: "could not find any record" });
-        }
-
-        return response.json(records);
-    } catch (failure) {
-        console.error(`failed to get all records: ${failure}`);
-
-        return response.status(400).json({ error: "could not find records" });
+  try {
+    const records = await Record.findAll();
+    if (!records.length) {
+      return response.status(204).json({ error: "could not find any record" });
     }
+
+    return response.json(records);
+  } catch (failure) {
+    console.error(`failed to get all records: ${failure}`);
+
+    return response.status(400).json({ error: "could not find records" });
+  }
 }
 
 async function createRecord(request, response) {
-    const record = ({
-        register_number,
-        inclusion_date,
-        city,
-        state,
-        requester,
-        document_type,
-        document_number,
-        document_date,
-        description,
-        sei_number,
-        receipt_form,
-        contact_info,
-        created_by,
-    } = request.body);
+  const record = ({
+    register_number,
+    inclusion_date,
+    city,
+    state,
+    requester,
+    document_type,
+    document_number,
+    document_date,
+    description,
+    sei_number,
+    receipt_form,
+    contact_info,
+    created_by,
+  } = request.body);
 
-    try {
-        if (Number.parseInt(record.created_by, 10) < 1) {
-            throw new Error("created_by -> invalid user id");
-        }
-
-        record.register_number = generateRegisterNumber();
-        record.inclusion_date = new Date();
-
-        const createdRecord = await Record.create(record);
-
-        if (!createdRecord) {
-            return response.status(500).json({ error: "could not create record" });
-        }
-
-        createdRecord.createSituation({ status: recordStatus.StatusPending });
-
-        return response.status(200).json(createdRecord);
-    } catch (error) {
-        return response
-            .status(500)
-            .json({ error: `could not insert record into database: ${error}` });
+  try {
+    if (Number.parseInt(record.created_by, 10) < 1) {
+      throw new Error("created_by -> invalid user id");
     }
+
+    record.register_number = generateRegisterNumber();
+    record.inclusion_date = new Date();
+
+    const createdRecord = await Record.create(record);
+
+    createdRecord.createSituation({ status: recordStatus.StatusPending });
+
+    return response.status(200).json(createdRecord);
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ error: `could not insert record into database: ${error}` });
+  }
+}
+
+async function getRecordsByPage(req, res) {
+  const { page } = req.params;
+  const itemsPerPage = 4;
+
+  try {
+    const { rows, count } = await Record.findAndCountAll({
+      limit: itemsPerPage,
+      offset: page,
+    });
+
+    if (count === 0) {
+      return res.status(204).json({ info: "there are no records matching this query" });
+    }
+
+    return res.status(200).json(rows);
+  } catch (err) {
+    console.error(`error: ${err}`);
+    return res.status(500).json({ error: "could not get records" });
+  }
 }
 
 module.exports = {
-    getRecordByID,
-    getAllRecords,
-    createRecord,
+  getRecordByID,
+  getAllRecords,
+  createRecord,
+  getRecordsByPage,
 };
