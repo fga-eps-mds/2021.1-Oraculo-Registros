685508247fcdbda5e4aab13eed0ec6066a1c9c4b
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2021-10-14 02:14:48 -0300

    fga-eps-mds/2021.1-Oraculo-Processos#101 added endpoint to update record status
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- src/Controller/RecordController.js
+++ src/Controller/RecordController.js
@@ -13,7 +13,9 @@ async function getRecordByID(request, response) {
 
   const record = await Record.findByPk(id);
   if (!record) {
-    return response.status(400).json({ error: `Could not find record with id ${id}` });
+    return response
+      .status(400)
+      .json({ error: `Could not find record with id ${id}` });
   }
 
   return response.json(record);
@@ -82,7 +84,9 @@ async function getRecordsByPage(req, res) {
     });
 
     if (count === 0) {
-      return res.status(204).json({ info: "there are no records matching this query" });
+      return res
+        .status(204)
+        .json({ info: "there are no records matching this query" });
     }
 
     return res.status(200).json(rows);
@@ -109,7 +113,9 @@ async function forwardRecord(req, res) {
 
   await record.addSection(section);
 
-  return res.status(200).json({ message: `record forwared to: ${section.name} ` });
+  return res
+    .status(200)
+    .json({ message: `record forwared to: ${section.name} ` });
 }
 
 async function getRecordSectionsByID(req, res) {
@@ -132,6 +138,21 @@ async function getRecordSectionsByID(req, res) {
   return res.status(200).json(record.sections);
 }
 
+async function setRecordSituation(req, res) {
+  const { id } = req.params;
+  const { situation_record } = req.body;
+
+  const saveResult = await Record.update(
+    { situation: situation_record },
+    { where: { id: id } }
+  );
+  console.log(saveResult);
+  if (!saveResult) {
+    return res.status(500).json({ error: "failed to save" });
+  }
+  return res.status(200).json("Successfully changed situation");
+}
+
 module.exports = {
   getRecordByID,
   getAllRecords,
@@ -139,4 +160,5 @@ module.exports = {
   forwardRecord,
   getRecordSectionsByID,
   getRecordsByPage,
+  setRecordSituation,
 };
--- src/routes.js
+++ src/routes.js
@@ -9,5 +9,6 @@ routes.post("/records", RecordController.createRecord);
 routes.get("/records/page/:page", RecordController.getRecordsByPage);
 routes.post("/records/:id/forward", RecordController.forwardRecord);
 routes.get("/records/:id/sections", RecordController.getRecordSectionsByID);
+routes.post("/records/:id/status", RecordController.setRecordSituation);
 
 module.exports = routes;
