401edccca8f578253f724296dbfaf8f5bc318c20
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-10-17 21:18:46 -0300

    fga-eps-mds/2021.1-Oraculo#107 feat: :sparkles: adds model to store field information
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

---
+++ src/Model/Field.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Field extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        /**
+         * Nome do campo
+         */
+        name: { type: DataTypes.TEXT },
+        /**
+         * nome exato do campo na tabela de registros
+         */
+        db_field_name: { type: DataTypes.TEXT },
+        /**
+         * Descrição do campo
+         */
+        description: { type: DataTypes.TEXT },
+        /**
+         * possui o id do usuário que criou o campo. Terá o valor 0 caso seja um campo default
+         */
+        created_by: { type: DataTypes.INTEGER },
+      },
+      {
+        sequelize,
+        tableName: "records.fields",
+      }
+    );
+  }
+}
+
+module.exports = Field;
