df74c5dc19937040df96fd07d007079ce8173bb5
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-10-17 21:22:16 -0300

    fga-eps-mds/2021.1-Oraculo#107 feat: :sparkles: add route to get record fields
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const RecordController = require("./Controller/RecordController");
 const routes = express.Router();
 
 routes.get("/records", RecordController.getAllRecords);
+routes.get("/records/fields", RecordController.getFields);
 routes.get("/records/:id", RecordController.getRecordByID);
 routes.post("/records", RecordController.createRecord);
 routes.get("/records/page/:page", RecordController.getRecordsByPage);
