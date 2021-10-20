6f1798e7b0eb12ec14d978e4531fa593f7b019dc
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2021-10-20 04:48:56 -0300

    fga-eps-mds/2021.1-Oraculo#112 fixed record model table erro
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- src/Model/Record.js
+++ src/Model/Record.js
@@ -1,39 +1,45 @@
 const { Model, DataTypes } = require("sequelize");
 
 class Record extends Model {
-    static init(sequelize) {
-        super.init(
-            {
-                register_number: { type: DataTypes.TEXT },
-                inclusion_date: { type: DataTypes.DATE },
-                city: { type: DataTypes.TEXT },
-                state: { type: DataTypes.TEXT },
-                requester: { type: DataTypes.TEXT },
-                document_type: { type: DataTypes.TEXT },
-                document_number: { type: DataTypes.TEXT },
-                document_date: { type: DataTypes.TEXT },
-                description: { type: DataTypes.TEXT },
-                sei_number: { type: DataTypes.TEXT },
-                receipt_form: { type: DataTypes.TEXT },
-                contact_info: { type: DataTypes.TEXT },
-                situation: { type: DataTypes.INTEGER },
-                created_by: { type: DataTypes.INTEGER },
-            },
-            {
-                sequelize,
-                tableName: "records",
-            }
-        );
-    }
+  static init(sequelize) {
+    super.init(
+      {
+        register_number: { type: DataTypes.TEXT },
+        inclusion_date: { type: DataTypes.DATE },
+        city: { type: DataTypes.TEXT },
+        state: { type: DataTypes.TEXT },
+        requester: { type: DataTypes.TEXT },
+        document_type: { type: DataTypes.TEXT },
+        document_number: { type: DataTypes.TEXT },
+        document_date: { type: DataTypes.TEXT },
+        description: { type: DataTypes.TEXT },
+        sei_number: { type: DataTypes.TEXT },
+        receipt_form: { type: DataTypes.TEXT },
+        contact_info: { type: DataTypes.TEXT },
+        situation: { type: DataTypes.INTEGER },
+        created_by: { type: DataTypes.INTEGER },
+      },
+      {
+        sequelize,
+        tableName: "records",
+      }
+    );
+  }
 
-    static associate(models) {
-        this.hasOne(models.Situation, { foreignKey: "record_id" });
-        this.belongsToMany(models.Section, {
-            foreignKey: "record_id",
-            through: "record_sections",
-            as: "sections",
-        });
-    }
+  static associate(models) {
+    this.hasOne(models.Situation, { foreignKey: "record_id" });
+    this.belongsToMany(models.Section, {
+      foreignKey: "record_id",
+      through: "record_sections",
+      as: "sections",
+    });
+
+    this.belongsToMany(models.History, {
+      foreignKey: "record_id",
+      through: "history_records",
+      as: "history",
+    });
+  }
 }
 
 module.exports = Record;
