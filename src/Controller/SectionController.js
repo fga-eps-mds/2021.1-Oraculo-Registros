4988a650820b50d358a2920c93ad993080c808a7
Lucas Vieira <lucas.engen@outlook.com>
2021-11-06 00:28:12 -0300

    chore: fixes indent fga-eps-mds/2021.1-Oraculo#164
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Controller/SectionController.js
+++ src/Controller/SectionController.js
@@ -2,9 +2,7 @@ const { Section } = require("../Model/Section");
 
 async function listSections(req, res) {
   const sections = await Section.findAll({
-    include: {
-      attributes: ["id", "name"],
-    },
+    attributes: ["id", "name"],
   });
 
   return res.status(200).json(sections);
