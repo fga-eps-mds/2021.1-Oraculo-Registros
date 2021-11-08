22c90ed75c2f45192e63d410e89d96f4019f4292
Lucas Vieira <lucas.engen@outlook.com>
2021-11-07 23:36:47 -0300

    chore: adds reopening and closing history to history table fga-eps-mds/2021.1-Oraculo#170
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Database/migrations/20211019041645-create-history.js
+++ src/Database/migrations/20211019041645-create-history.js
@@ -15,19 +15,35 @@ module.exports = {
       },
       origin_name: {
         type: Sequelize.TEXT,
-        allowNull: false,
+        allowNull: true,
       },
       destination_id: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        allowNull: true,
       },
       destination_name: {
         type: Sequelize.TEXT,
-        allowNull: false,
+        allowNull: true,
       },
       forwarded_by: {
         type: Sequelize.TEXT,
-        allowNull: false,
+        allowNull: true,
+      },
+      closed_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      closed_by: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      reopened_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      reopened_by: {
+        type: Sequelize.TEXT,
+        allowNull: true,
       },
       record_id: {
         type: Sequelize.INTEGER,
