c75a6eafdc56bf4b74edb410aadbebc99187ec26
Lucas Vieira <lucas.engen@outlook.com>
2021-11-07 23:36:05 -0300

    refactor: adds getters to omit empty fields fga-eps-mds/2021.1-Oraculo#170
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Model/History.js
+++ src/Model/History.js
@@ -7,11 +7,105 @@ class History extends Model {
         /**
          * Contém o email do usuário que encaminhou o registro
          */
-        forwarded_by: { type: DataTypes.TEXT },
-        origin_id: { type: DataTypes.INTEGER },
-        origin_name: { type: DataTypes.TEXT },
-        destination_id: { type: DataTypes.INTEGER },
-        destination_name: { type: DataTypes.TEXT },
+        origin_id: {
+          type: DataTypes.INTEGER,
+          get() {
+            const val = this.getDataValue("origin_id");
+            if (!val) {
+              return;
+            }
+
+            return val;
+          },
+        },
+        origin_name: {
+          type: DataTypes.TEXT,
+          get() {
+            const val = this.getDataValue("origin_name");
+            if (!val) {
+              return;
+            }
+
+            return val;
+          },
+        },
+        destination_id: {
+          type: DataTypes.INTEGER,
+          get() {
+            const val = this.getDataValue("destination_id");
+            if (!val) {
+              return;
+            }
+
+            return val;
+          },
+        },
+        destination_name: {
+          type: DataTypes.TEXT,
+          get() {
+            const val = this.getDataValue("destination_name");
+            if (!val) {
+              return;
+            }
+
+            return val;
+          },
+        },
+        forwarded_by: {
+          type: DataTypes.TEXT,
+          get() {
+            const val = this.getDataValue("forwarded_by");
+            if (!val) {
+              return;
+            }
+
+            return val;
+          },
+        },
+        closed_at: {
+          type: DataTypes.DATE,
+          get() {
+            const val = this.getDataValue("closed_at");
+            if (!val) {
+              return;
+            }
+
+            return val;
+          },
+        },
+        closed_by: {
+          type: DataTypes.TEXT,
+          get() {
+            const val = this.getDataValue("closed_by");
+            if (!val) {
+              return;
+            }
+
+            return val;
+          },
+        },
+        reopened_at: {
+          type: DataTypes.DATE,
+          get() {
+            const val = this.getDataValue("reopened_at");
+            if (!val) {
+              return;
+            }
+
+            return val;
+          },
+        },
+        reopened_by: {
+          type: DataTypes.TEXT,
+          get() {
+            const val = this.getDataValue("reopened_by");
+            if (!val) {
+              return;
+            }
+
+            return val;
+          },
+        },
       },
       {
         sequelize,
